612abddb941125f921e4ced86f006aaa7de30af8
Felix Brousseau-Tremblay <f.brousseau-tremblay@klarna.com>
2021-12-14 17:18:24 +0100

    Refactor day2

--- day2.ml
+++ day2.ml
@@ -1,12 +1,41 @@
 open Core
 open List
 
-let parse_line line =
-  let direction = String.split ~on:' ' line in
-  match direction with
-  | [ "forward"; value ] -> int_of_string value
-  | [ "down"; value ] -> int_of_string value
-  | [ "up"; value ] -> int_of_string value
-  | _ -> 0
+let test_input =
+  [ "forward 5"; "down 5"; "forward 8"; "up 3"; "down 8"; "forward 2" ]
 
-let parse_to_command filename = filename |> In_channel.read_lines >>| fun x -> x
+let input = "public/day2.txt" |> In_channel.read_lines
+
+let parse_line line =
+  match String.split ~on:' ' line with
+  | [ dir; n ] -> (dir, int_of_string n)
+  | _ -> raise (Invalid_argument line)
+
+let calculate_position (x, y) (dir, n) =
+  match dir with
+  | "forward" -> (x + n, y)
+  | "up" -> (x, y - n)
+  | "down" -> (x, y + n)
+  | _ -> raise (Invalid_argument dir)
+
+let solve directions =
+  directions >>| parse_line |> List.fold_left ~init:(0, 0) ~f:calculate_position
+  |> fun (x, y) -> x * y
+
+let () = Profile.profile solve test_input |> Printf.printf "Result -> %d\n\n"
+
+let () = Profile.profile solve input |> Printf.printf "Result -> %d\n\n"
+
+let calculate_position' (x, y, aim) (dir, n) =
+  match dir with
+  | "forward" -> (x + n, y + (aim * n), aim)
+  | "up" -> (x, y, aim - n)
+  | "down" -> (x, y, aim + n)
+  | _ -> raise (Invalid_argument dir)
+
+let solve' directions =
+  directions >>| parse_line
+  |> List.fold_left ~init:(0, 0, 0) ~f:calculate_position'
+  |> fun (x, y, _) -> x * y
+
+let () = Profile.profile solve' input |> Printf.printf "Result -> %d\n\n"
